8bd75b7580324eb50dc715d935629784f78bd2c9
Rene Ramirez <rdejramirez@gmail.com>
2022-09-07 17:36:04 -0500

    discord chatbot first steps

---
+++ deploy-commands.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder, Routes } = require('discord.js')
+const { REST } = require('@discordjs/rest')
+
+const { clientId, guildId, token } = require('./config.json')
+
+const commands = [
+  new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('Responde con un pong'),
+  new SlashCommandBuilder()
+    .setName('nombre')
+    .setDescription('Responde con un nombre'),
+].map((command) => command.toJSON())
+
+const rest = new REST({
+  version: '10',
+}).setToken(token)
+
+const deployCommands = async () => {
+  try {
+    await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
+      body: commands,
+    })
+    console.log('Commands deployed sucessfully')
+  } catch (e) {
+    console.log('Error', e)
+  }
+}
+
+deployCommands()
---
+++ index.js
@@ -0,0 +1,23 @@
+const { Client, GatewayIntentBits } = require('discord.js')
+
+//requerir token
+const { token } = require('./config.json')
+
+const superBot = new Client({ intents: GatewayIntentBits.Guilds })
+
+superBot.once('ready', () => {
+  console.log('Super bot en linea y listo para iniciar 😎')
+})
+
+superBot.on('interactionCreate', async (interaction) => {
+  if (!interaction.isChatInputCommand) return
+  const { commandName } = interaction
+  if (commandName === 'ping') {
+    await interaction.reply('pong pong')
+  } else if (commandName === 'nombre') {
+    await interaction.reply('René Ramírez')
+  }
+})
+
+//logueamos con nuestro token
+superBot.login(token)
